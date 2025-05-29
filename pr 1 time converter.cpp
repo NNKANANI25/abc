66985e8892d6160353813f1d60ecaced6cbbdc2a
NNKANANI25 <bhavnamkanani@gmail.com>
2025-05-29 15:50:08 +0530

    Add files via upload

---
+++ pr 1 time converter.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+using namespace std;
+void second()
+{
+	int second;
+	cout<<"enter totalsecond:";
+	cin>>second;
+	int h,m,s;
+	h=second/3600;
+	m=second%3600/60;
+	s=second%60;
+	cout<<"HH:MM:SS=>"<<h<<":"<<m<<":"<<s;
+	cout<<endl; 
+}
+void hour()
+{
+	int hour,min,sec;
+	cout<<"enter hour: ";
+	cin>>hour;
+	cout<<"enter min: ";
+	cin>>min;
+	cout<<"enter sec: ";
+	cin>>sec;
+	int h=(hour*3600)+(min*60)+(sec);
+	cout<<"total seconds: "<<h;
+}
+int main()
+{
+	second();
+	hour();
+}
+/*OUTPUT
+*/
